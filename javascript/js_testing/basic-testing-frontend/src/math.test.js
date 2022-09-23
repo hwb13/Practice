7fa45123e1957d839c48ffeaffcc36d438225432
Hunter <hbann29@protonmail.com>
2022-09-22 21:16:11 -0400

    9/22/2022: Testing for errors

--- javascript/js_testing/basic-testing-frontend/src/math.test.js
+++ javascript/js_testing/basic-testing-frontend/src/math.test.js
@@ -3,6 +3,43 @@ import { it, expect } from "vitest";
 import { add } from "./math.js";
 
 it("should summarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  const numbers = [1, 2, 3];
+  const expectedResult = 6;
+  const result = add(numbers);
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+  const result = add(inputs);
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array ofnumeric string values is provided", () => {
+  const numbers = ["1", "2"];
+  const result = add(numbers);
+  const expectedResult = 3;
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+  const result = add(numbers);
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function", () => {
+  function resultFn() {
+    add();
+  }
+  expect(resultFn).toThrow();
+});
+
+it("should throw an error if provided with multiple arguments instead of an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+  function resultFn() {
+    add(num1, num2);
+  }
+  expect(resultFn).toThrow();
 });
